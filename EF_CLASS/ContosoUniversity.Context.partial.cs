606a9901233befd5791ad2276484ee05713e0906
codyidle <cody@edetw.com>
2016-06-08 16:10:18 +0800

    第一次簽入備份

---
+++ EF_CLASS/ContosoUniversity.Context.partial.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_CLASS
+{
+    public partial class ContosoUniversityEntities : DbContext
+    {
+        public override int SaveChanges()
+        {
+            var entities = this.ChangeTracker.Entries();
+            foreach (var entry in entities)
+            {
+                if (entry.Entity is Course && entry.State == EntityState.Modified)
+                {
+                    entry.CurrentValues.SetValues(new
+                    {
+                        ModifyTime = DateTime.Now
+                    });
+                }
+            }
+
+
+            return base.SaveChanges();
+        }
+    }
+}
---
+++ EF_CLASS/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_CLASS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new ContosoUniversityEntities())
+            {
+                db.Database.Log = Console.WriteLine;
+                var c = db.Department.Find(1);
+                c.Name += "!!";
+                db.SaveChanges();
+            }
+
+
+
+            //var c = new Course()
+            //{
+            //    CourseID = 0,
+            //    Credits = 1,
+            //    //CreateTime = DateTime.Now,
+            //    ModifyTime = DateTime.Now,
+            //    DepartmentID = 2,
+            //    Title = "Entity Framework 6 @ Ede"
+
+            //};
+
+            //using (var db1 = new ContosoUniversityEntities())
+            //{
+            //    c = db1.Course.Find(6);
+            //    c.Title += "!!";
+            //}
+
+            //using (var db2 = new ContosoUniversityEntities())
+            //{
+            //    //db2.Course.Attach(c);
+            //    db2.Entry(c).State = System.Data.Entity.EntityState.Modified;
+            //    db2.SaveChanges();
+            //}
+
+
+            using (var db = new ContosoUniversityEntities())
+            {
+
+
+
+
+
+                //db.Database.Log = msg =>
+                //{
+                //    Console.WriteLine(msg);
+                //};
+
+
+                ////var c =db.Course.Find(5);
+                ////c.Credits += 1;
+                ////db.SaveChanges();
+
+                //var addcurse = new Course()
+                //{
+                //    CourseID = 0,
+                //    Credits = 1,
+                //    CreateTime = DateTime.Now,
+                //    //ModifyTime = DateTime.Now,
+                //    DepartmentID = 2,
+                //    Title = "Entity Framework 6 @ Ede"
+
+                //};
+                //db.Course.Add(addcurse);
+
+                //var c = db.Course.Find(2);
+                //c.CreateTime = DateTime.Now;
+                //c.Credits += 1;
+
+                //db.SaveChanges();
+            }
+        }
+    }
+}
